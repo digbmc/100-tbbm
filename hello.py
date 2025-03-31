29e03648eb23b265b8928ef593b31cdabd105367
Alice Tweedy McGrath <atmcgrath@gmail.com>
2025-03-31 17:48:38 -0400

    apply to main page

--- hello.py
+++ hello.py
@@ -15,10 +15,9 @@ def add_security_headers(response):
 #     return response
 
 @app.route("/")
-def hello_world():
-    return render_template("hello.html")
-
-@app.route("/data")
+# def hello_world():
+#     return render_template("hello.html")
+# @app.route("/data")
 def get_json():
     data_dir = os.path.join(os.path.dirname(__file__), 'data')
     filename = os.path.join(data_dir, 'bmc.json')
@@ -31,24 +30,6 @@ def get_json():
                 p = (entry[key][0], entry[key][r])
                 pairs.append(p)
         return render_template("hello.html", entry=pairs)
-
-        # return ' '.join(data['sheets'][0]['62073537'])
-
-
-"""
-        entry = data['sheets'][0]
-        pairs = []
-        for key in entry.keys():
-            pair = (entry[key][0], entry[key][1])
-            pairs.append(pair)
-    pairs = []
-    for item in data['sheets'][0]:
-        category = item[0]
-        value = item[1]
-        pairs.append((category, value))
-    return render_template("hello.html", entry=pairs)
-"""    
-    # return data['sheets'][0]['62073537'][0]
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
