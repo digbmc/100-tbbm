c3c94042aeccaf45ae267ba0618aeaadf35c30af
Alice Tweedy McGrath <atmcgrath@gmail.com>
2025-03-31 17:43:10 -0400

    commented out unused functions

--- static/js/main.js
+++ static/js/main.js
@@ -9,26 +9,19 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var button = document.getElementById('colorButton');
-//     button.addEventListener('click', function() {
-//         var colors = ['pink', 'purple', 'red'];
-//         var randomColor = colors[Math.floor(Math.random() * colors.length)];
-//         button.style.backgroundColor = randomColor;
-//     });
-// });
+
 
 
 // document.getElementById('demo').innerText = 'Hello JavaScript!';
 
-function test() {
-    document.getElementById('demo').innerText = 'Hello JavaScript!';
-};
+// function test() {
+//     document.getElementById('demo').innerText = 'Hello JavaScript!';
+// };
 
 
-function reload(){
-    Window.location.reload();
-};
+// function reload(){
+//     Window.location.reload();
+// };
 
 
 
