e4969b7659744d5b8b6f85ec615b9d9d40c3d5b0
Alice Tweedy McGrath <atmcgrath@gmail.com>
2025-06-04 16:10:36 -0400

    added alumni data, moved other to data folder, testing sleep function

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ import requests
 from flask_caching import Cache
 import csv
 import json
+import time
 
 app = Flask(__name__)
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
@@ -76,19 +77,8 @@ def request_data():
     return render_template("hello.html", entry=pairs)
 
 @app.route("/options") # has values for main event
-# def request_options(): # for live loading
-#     options = cache.get('option_data')
-#     if options is None:
-#         sheet_gids = [1202051472, 1009788276, 1362786704, 2071309954, 865859103,
-#             60204872, 1356126397, 1552947615, 1430527252, 427379088, 299682830,
-#             1641709279, 1638835585, 1425001770]
-#         sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIKHYbU9rAl4uAlZcy-L1OEAgbYMCyp_3mamMxQH-PyIcR6IkWWmy6GTN9MhNNSmcd9KPhWK6KumL-/pub?output=csv"
-#         options = get_permutations(sheet_gids, sheet_url)
-#         cache.set('option_data', options, timeout=100)
-#     responses = get_random(options)
-#     return render_template("options.html", cl='options', entry=zip(questions, responses))
 def get_json(): #for static loading
-    json_path = os.path.join(app.root_path, 'static', 'april-5-data.json')
+    json_path = os.path.join(app.root_path, 'data', 'april-5-data.json')
     with open(json_path, 'r') as test_file:
         data = json.load(test_file)
         pairs = []
@@ -100,23 +90,17 @@ def get_json(): #for static loading
         return render_template("options.html", cl='options', entry=pairs)
 
 @app.route("/alumni")
-def request_alumni():
-    alumni = cache.get('alumni_data')
-    if alumni is None:
-        sheet_gids = [785551783, 723501988, 928268027, 861216909, 953776574, 476395773, 1967688672]
-        sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQgaiDTOlSJYQyZSafSTLpx_LfjSPHGteEL5b6GDP9ar0VQS6dklT7JwQFTrUyRITYvxiiyRsnHKBiA/pub?output=csv"
-        alumni = get_permutations(sheet_gids, sheet_url)
-        cache.set('alumni_data', alumni, timeout=100)
-    responses = get_random(alumni)
-    alum_questions = [
-        'During their first year, students ….',
-        'Students develop and demonstrate their quantitative skills …',
-        'Students develop language skills….',
-        'Students achieve the breadth expected of a liberal arts education …',
-        'Students practice interdisciplinary thinking …',
-        'Our undergraduate credentialing includes …',
-        'Seniors integrate their college experience and demonstrate their mastery of their academic program…']
-    return render_template("options.html", cl='alum', entry=zip(alum_questions, responses))
+def get_alum_json(): #for static loading
+    json_path = os.path.join(app.root_path, 'data', 'alum-data.json')
+    with open(json_path, 'r') as test_file:
+        data = json.load(test_file)
+        pairs = []
+        for entry in data:
+            question = entry['question']
+            r = random.randint(0,9)
+            answer = entry['options'][r]
+            pairs.append((question, answer))
+        return render_template("options.html", cl='alum', entry=pairs)
 
 @app.route("/board")
 def request_board():
@@ -142,18 +126,52 @@ def request_board():
     responses = get_random(board)
     return render_template("options.html", cl='options', entry=zip(questions, responses))
 
-# @app.route("/test")
-# def get_json():
-#     json_path = os.path.join(app.root_path, 'static', 'april-5-data.json')
-#     with open(json_path, 'r') as test_file:
-#         data = json.load(test_file)
-#         pairs = []
-#         for entry in data:
-#             question = entry['question']
-#             r = random.randint(0,9)
-#             answer = entry['options'][r]
-#             pairs.append((question, answer))
-#         return render_template("options.html", cl='options', entry=pairs)
+@app.route("/test")
+def get_test_json(): #for static loading
+    json_path = os.path.join(app.root_path, 'data', 'alum-data.json')
+    with open(json_path, 'r') as test_file:
+        data = json.load(test_file)
+        pairs = []
+        for entry in data:
+            question = entry['question']
+            r = random.randint(0,9)
+            answer = entry['options'][r]
+            pairs.append((question, answer))
+        time.sleep(.5)
+        return render_template("options.html", cl='alum', entry=pairs)
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=8000, debug=True)
+
+
+"""
+def request_alumni():
+    alumni = cache.get('alumni_data')
+    if alumni is None:
+        sheet_gids = [785551783, 723501988, 928268027, 861216909, 953776574, 476395773, 1967688672]
+        sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQgaiDTOlSJYQyZSafSTLpx_LfjSPHGteEL5b6GDP9ar0VQS6dklT7JwQFTrUyRITYvxiiyRsnHKBiA/pub?output=csv"
+        alumni = get_permutations(sheet_gids, sheet_url)
+        cache.set('alumni_data', alumni, timeout=100)
+    responses = get_random(alumni)
+    alum_questions = [
+        'During their first year, students ….',
+        'Students develop and demonstrate their quantitative skills …',
+        'Students develop language skills….',
+        'Students achieve the breadth expected of a liberal arts education …',
+        'Students practice interdisciplinary thinking …',
+        'Our undergraduate credentialing includes …',
+        'Seniors integrate their college experience and demonstrate their mastery of their academic program…']
+    return render_template("options.html", cl='alum', entry=zip(alum_questions, responses))
+
+# def request_options(): # for live loading
+#     options = cache.get('option_data')
+#     if options is None:
+#         sheet_gids = [1202051472, 1009788276, 1362786704, 2071309954, 865859103,
+#             60204872, 1356126397, 1552947615, 1430527252, 427379088, 299682830,
+#             1641709279, 1638835585, 1425001770]
+#         sheet_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRIKHYbU9rAl4uAlZcy-L1OEAgbYMCyp_3mamMxQH-PyIcR6IkWWmy6GTN9MhNNSmcd9KPhWK6KumL-/pub?output=csv"
+#         options = get_permutations(sheet_gids, sheet_url)
+#         cache.set('option_data', options, timeout=100)
+#     responses = get_random(options)
+#     return render_template("options.html", cl='options', entry=zip(questions, responses))    
+"""
